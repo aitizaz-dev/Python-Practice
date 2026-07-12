e5962d0e872b3367d67cca53a7b4d8e7377d27ec
aitizaz-dev <aitizazhassan242@gmail.com>
2026-07-12 23:19:39 +0500

    Add files via upload

---
+++ Day03 . data types .py.py
@@ -0,0 +1,28 @@
+# Day 03 . Data type & Type casting
+
+
+name = "Aitizaz Hassan"
+age = 18
+height = 5.6
+is_student = True
+
+# originol value
+
+print("Name:",name)
+print("Age:",age)
+print("Height:",height)
+print("Student:",is_student)
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+
+# Type casting
+
+age = float(age)
+
+print("Age:",age)
+print("Data type:",type(age))
+
+
