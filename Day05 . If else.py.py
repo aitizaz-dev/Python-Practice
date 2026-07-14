edd833a3c2ce29015ad7d0502b84402c36845a05
aitizaz-dev <aitizazhassan242@gmail.com>
2026-07-14 17:59:09 +0500

    Add files via upload

---
+++ Day05 . If else.py.py
@@ -0,0 +1,9 @@
+
+print("===If_Else Statements===")
+
+print("===Age Eligibilty Checker===")
+age = int(input("Enter your age:"))
+if age >= 18:
+    print("Congratulations! You are eligible to vote.")
+else:
+    print("Sorry! You are not eligible to vote.")
