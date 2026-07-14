2cf0b7a0ce135813543f7c8b7b67d8bb0c12d5dd
aitizaz-dev <aitizazhassan242@gmail.com>
2026-07-14 16:30:22 +0500

    Add files via upload

---
+++ Day04 . Operators.py.py
@@ -0,0 +1,30 @@
+# Day 04 Operators in python
+print("==== python operators ====")
+
+a = 10
+b = 30
+
+print("===Arithmetic operators===")
+
+print("Addition:",a+b)
+print("Subtraction:",a-b)
+print("Multiplication:",a*b)
+print("Division:",a/b)
+print("Floor division:",a//b)
+print("Modulus:",a%b)
+print("Power:",a**b)
+
+print("===Comparision Operators===")
+
+print("a==b:",a==b)
+print("a!=b:",a!=b)
+print("a>b:",a>b)
+print("a<b:",a<b)
+print("a>=b:",a>=b)
+print("a<=b:",a<=b)
+
+print("===Logical Operators===")
+
+print((a>10)and(b<10))
+print((a<20)or(b>10))
+print(not(a>10))
