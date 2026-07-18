86c8cd8d8251b0cd5ce6e0e0c942d11b2721fb5c
aitizaz-dev <aitizazhassan242@gmail.com>
2026-07-18 13:16:42 +0500

    Add files via upload

---
+++ Day08 . while_loop.py
@@ -0,0 +1,30 @@
+# day08  While loop 
+# Programe . 01
+
+print("===== Print  Numbers =====")
+
+number = 1
+while number <= 10:
+    print(number)
+    number += 1
+
+# programe . 02
+
+print("==== print countdown ====")
+count = 10
+while count >= 1:
+    print(count)
+    count -= 1
+
+print(" Blast off ")
+
+# Programe . 03 
+
+print("=== Password Checker ===")
+
+password = ""
+
+while password != "Mian123":
+    password = input("Enter Password:")
+
+print("Access Granted")
