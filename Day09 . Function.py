3758c48b4784134fb64af6470e29546019a7db96
aitizaz-dev <aitizazhassan242@gmail.com>
2026-07-19 15:36:40 +0500

    Add files via upload

---
+++ Day09 . Function.py
@@ -0,0 +1,39 @@
+# Day 09 - Functions
+
+# programe 1 Simple Function
+
+def welcome():
+    print("Welcome to Python!")
+    print("Keep Learning Every Day.")
+
+welcome()
+
+
+# Programe 2 Addition Function
+
+def add():
+    num1 = int(input("Enter Firsr Number:"))
+    num2 = int(input("Enter Second Number:"))
+    print("Sum =",num1+num2)
+
+add()
+
+
+# Program 3 Greeting Function
+
+def greet():
+    name = input("Enter Your Name:")
+
+    print("Hello,",name)
+    print("Welcome to Python Programming!")
+
+greet()
+
+# Progrme 4 Square of a Number
+
+def square():
+    number = int(input("Enter a Number:"))
+
+    print("square =",number * number)
+
+square()
