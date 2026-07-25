6a25d4ca566f0d4f40912c796cf8c96add0a322d
aitizaz-dev <aitizazhassan242@gmail.com>
2026-07-25 12:30:50 +0500

    Add files via upload

---
+++ Day11_Lists.py
@@ -0,0 +1,24 @@
+# Program 1 Create and Print a List
+
+fruits = ["Apple","Banana","Peach","Strawberry"]
+print("=== Fruit List ====")
+
+print(fruits)
+
+# Program 2 Access List Items
+
+fruits = ["Apple","Banana","Peach","Strawberry"]
+
+print("First Fruit:",fruits[0])
+print("Second Fruit:",fruits[1])
+print("Last Fruit:",fruits[-1])
+
+
+# Program 3 Change List Item
+
+fruits = ["Apple","Banana","Peach","Strawberry"]
+
+fruits[1] = "Mango"
+
+print("Update List")
+print(fruits)
